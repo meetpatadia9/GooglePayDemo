27a0ffd9e924e5d9995f24ce482a6c254a82d1fd
IPS-030 <meetp-itpath@gmail.com>
2024-02-06 09:45:18 +0530

    DONE: -[X] URI intent to payment screen
    ISSUE:
    -[X] Payment Failed: To protect your money this payment was declined.

--- app/src/main/java/com/ipsmeet/googlepaydemo/MainActivity.kt
+++ app/src/main/java/com/ipsmeet/googlepaydemo/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.ipsmeet.googlepaydemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ipsmeet.googlepaydemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val GOOGLE_PAY_PACKAGE_NAME = "com.google.android.apps.nbu.paisa.user"
+    private val GOOGLE_PAY_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnPayWithGPay.setOnClickListener {
+            payViaGPay()
+        }
+    }
+
+    private fun payViaGPay() {
+        val uri = Uri.Builder()
+            .scheme("upi")
+            .authority("pay")
+            .appendQueryParameter("pa", getString(R.string.receiver_upi_id)) // UPI-ID of business account only
+            .appendQueryParameter("pn", getString(R.string.receiver_name))
+            .appendQueryParameter("am", "1")
+            .appendQueryParameter("cu", "INR")
+            .appendQueryParameter("tn", "testing")
+            .appendQueryParameter("mc", System.currentTimeMillis().toString())
+            .appendQueryParameter("tr", System.currentTimeMillis().toString())
+            .build()
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.setData(uri)
+        intent.setPackage(GOOGLE_PAY_PACKAGE_NAME)
+        startActivity(intent)
     }
 }
